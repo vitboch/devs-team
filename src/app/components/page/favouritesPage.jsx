ee4d2e432d81bb760fdd26da5b5a83f21ada29cd
unknown <you@example.com>
2023-01-22 00:16:52 +0600

    footer, info

--- src/app/components/page/favouritesPage.jsx
+++ src/app/components/page/favouritesPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const FavouritesPage = () => {
-    const favouritesIds = JSON.parse(localStorage.getItem("favourites"));
     return (
         <>
             <h1 className="fw-light text-center mb-4">Избранное</h1>
