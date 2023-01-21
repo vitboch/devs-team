7281b73d9a845bfd6f185818d87e75a2c7f6cf05
Vitaly Bochkarev <vitalybochkarev@Book.local>
2023-01-21 22:51:25 +0700

    add card

--- src/app/components/common/button.jsx
+++ src/app/components/common/button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Button = ({ size, color, label, onClick, type, rounded, disabled, icon }) => {
+const Button = ({
+    size,
+    color,
+    label,
+    onClick,
+    type,
+    rounded,
+    disabled,
+    icon
+}) => {
     return (
         <button
             type={type}
@@ -17,14 +26,24 @@ Button.defaultProps = {
     size: "",
     color: "primary",
     type: "button",
-    rounded: "",
+    rounded: "rounded-0",
     disabled: false
 };
 
 Button.propTypes = {
     type: PropTypes.oneOf(["button", "submit"]),
     size: PropTypes.oneOf(["btn-sm", "btn-lg"]),
-    color: PropTypes.oneOf(["primary", "secondary", "success", "danger", "warning", "info", "light", "dark", "link"]),
+    color: PropTypes.oneOf([
+        "primary",
+        "secondary",
+        "success",
+        "danger",
+        "warning",
+        "info",
+        "light",
+        "dark",
+        "link"
+    ]),
     rounded: PropTypes.oneOf(["rounded-0", "rounded-pill"]),
     label: PropTypes.string,
     onClick: PropTypes.func,
---
+++ src/app/components/common/card.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Button from "../common/button";
+
+const Card = ({ _id, firstName, lastName, birthday, photo, about }) => {
+    return (
+        <div className="col">
+            <div className="shadow-sm card">
+                <img src={photo} className="card-img-top" alt={photo} />
+                <div className="card-body">
+                    <h5>
+                        {firstName} {lastName}
+                    </h5>
+                    <p className="card-text">{birthday}</p>
+                    <span className="block-about card-text">{about}</span>
+                    <div className="container">
+                        <div className="d-flex justify-content-between align-items-center pb-3">
+                            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <Link to={`/devId/${_id}`}>
+                                    <Button
+                                        label="открыть"
+                                        size="btn-sm"
+                                        color="light"
+                                    />
+                                </Link>
+                                <Button
+                                    label="добавить в избранное"
+                                    size="btn-sm"
+                                    color="light"
+                                    onClick={() => console.log("Add id", _id)}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Card.propTypes = {
+    _id: PropTypes.string.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    birthday: PropTypes.string.isRequired,
+    photo: PropTypes.string.isRequired,
+    about: PropTypes.string.isRequired
+};
+
+export default Card;
