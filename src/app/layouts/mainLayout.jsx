280ba7d0729bdf17ad8aa4e814ed58861cc4f319
lukin <lukinykh.nikita@yandex.ru>
2023-01-21 17:33:54 +0300

    Changing the project structure

--- src/app/layouts/mainLayout.jsx
+++ src/app/layouts/mainLayout.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Breadcrumbs from "../components/ui/breadcrumbs";
 
 const MainLayout = () => {
-    return <Outlet />;
+    return (
+        <>
+            <Breadcrumbs />
+            <Outlet />
+        </>
+    );
 };
 
 export default MainLayout;
