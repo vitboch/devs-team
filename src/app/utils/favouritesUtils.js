ad3c3d9cfc63a5a8b4e029f2a98873a30f05cc7b
unknown <you@example.com>
2023-01-22 12:48:04 +0600

    .

---
+++ src/app/utils/favouritesUtils.js
@@ -0,0 +1,31 @@
+import devs from "../api/devs.api";
+
+export const getStorage = () => {
+    if (
+        localStorage.getItem("favourites") &&
+        localStorage.getItem("favourites") !== "[]"
+    ) {
+        return JSON.parse(localStorage.getItem("favourites")).map((id) =>
+            devs.find((dev) => dev._id === id)
+        );
+    } else return [];
+};
+
+export const changeFavouritesIds = (id) => {
+    if (
+        localStorage.getItem("favourites") &&
+        localStorage.getItem("favourites") !== "[]"
+    ) {
+        const favourites = JSON.parse(localStorage.getItem("favourites"));
+        const isFavourite = favourites.some((item) => item === id);
+        if (isFavourite) {
+            const newFavourites = favourites.filter((item) => item !== id);
+            localStorage.setItem("favourites", JSON.stringify(newFavourites));
+        } else {
+            favourites.push(id);
+            localStorage.setItem("favourites", JSON.stringify(favourites));
+        }
+    } else {
+        localStorage.setItem("favourites", JSON.stringify([id]));
+    }
+};
