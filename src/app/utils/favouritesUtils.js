b5f9b40f4ee2c1252e8a3713a40c0bf5c44d62da
unknown <you@example.com>
2023-01-22 18:23:49 +0600

    favourites, footer

--- src/app/utils/favouritesUtils.js
+++ src/app/utils/favouritesUtils.js
@@ -1,25 +1,24 @@
 export const getStorage = () => {
-    if (
-        localStorage.getItem("favourites")
-    ) {
+    if (localStorage.getItem("favourites")) {
         return JSON.parse(localStorage.getItem("favourites"));
     } else return [];
 };
 
 export const changeStorage = (id) => {
-    if (
-        localStorage.getItem("favourites")
-    ) {
+    if (localStorage.getItem("favourites")) {
         const favourites = JSON.parse(localStorage.getItem("favourites"));
         const isFavourite = favourites.some((item) => item === id);
         if (isFavourite) {
+            // если id есть в массиве - удаляю
             const newFavourites = favourites.filter((item) => item !== id);
             localStorage.setItem("favourites", JSON.stringify(newFavourites));
         } else {
+            // если id нет в массиве - добавляю
             favourites.push(id);
             localStorage.setItem("favourites", JSON.stringify(favourites));
         }
     } else {
+        // если в localStorage нет favourites - создаю и добавляю id
         localStorage.setItem("favourites", JSON.stringify([id]));
     }
 };
